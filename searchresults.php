c45c999d7d37dce51843bb955edba57f190ffb10
simon mbugua <smbugua11@gmail.com>
2018-04-22 16:55:03 +0300

    searches

--- searchresults.php
+++ searchresults.php
@@ -28,7 +28,6 @@ $exams=mysql_query("SELECT * FROM patients where idno='$idno' or tel='$phone'");
                   <th>Name</th>
                   <th>Tels</th>
                   <th>Email</th>
-                  <th>Labs</th>
                   <th>Bill</th>
                 </tr>
               </thead>
@@ -44,7 +43,6 @@ $exams=mysql_query("SELECT * FROM patients where idno='$idno' or tel='$phone'");
                   <td><?php echo $row['name']?></td>
                   <td><?php echo $row['tel']?></td>
                   <td><?php echo $row['email']?></td>
-                  <td><a href="patientoverview.php?id=<?php echo $row['id']?>" class="btn btn-primary btn-mini"><i class="icon icon-edit"></i> Labs</a> </td>
                   <td><a href="billclient.php?id=<?php echo $row['id']?>" class="btn btn-danger btn-mini"><i class="icon icon-money"></i> Bill</a> </td>
                 </tr>
                 <?php }?>
