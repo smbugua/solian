e408d78077ee051d9b6b2f217e5ec0cdbadba41f
simon mbugua <smbugua11@gmail.com>
2018-06-27 15:32:22 +0300

    ok kitu

---
+++ auth.php
@@ -0,0 +1,111 @@
+<?php // rnfunctions.php
+//error_reporting(E_ALL ^ E_DEPRECATED);
+ini_set('display_errors',0);
+$dbhost = 'localhost'; // Unlikely to require changing
+$dbname = 'solian'; // Modify these...
+$dbuser = 'root'; // ...variables according
+$dbpass = ''; // ...to your installation
+mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
+mysql_select_db($dbname) or die(mysql_error());
+function sessionStart()
+{
+session_start();
+}
+function createTable($name, $query)
+{
+if (tableExists($name))
+{
+echo "Table '$name' already exists<br />";
+}
+else
+{
+queryMysql("CREATE TABLE $name($query)");
+echo "Table '$name' created<br />";
+}
+}
+function tableExists($name)
+{
+$result = queryMysql("SHOW TABLES LIKE '$name'");
+return mysql_num_rows($result);
+}
+function queryMysql($query)
+{
+$result = mysql_query($query) or die(mysql_error());
+return $result;
+}
+function destroySession()
+{
+$_SESSION=array();
+if (session_id() != "" || isset($_COOKIE[session_name()]))
+setcookie(session_name(), '', time()-2592000, '/');
+session_destroy();
+}
+function sanitizeString($var)
+{
+$var = strip_tags($var);
+$var = htmlentities($var);
+$var = stripslashes($var);
+return mysql_real_escape_string($var);
+}
+
+
+function processQuery($query)
+{
+    
+    $result=queryMysql($query);
+    return $result;
+    
+}
+function getstock($id){
+$stockquery=mysql_fetch_array(mysql_query("SELECT totalstock from productstock where productid='$id' "));
+$stock=$stockquery['totalstock'];
+return $stock;
+}
+function updatestock($id,$newstock){
+mysql_query("UPDATE productstock set totalstock='$newstock' where productid='$id'");
+}
+
+function getInvoicePatient($invoiceid){
+	$invoicequery=mysql_fetch_array(mysql_query("SELECT patientid from invoices where id='$invoiceid' "));
+$p=$invoicequery['patientid'];
+return $p;
+}
+function getSmsCount(){
+	$stockquery=mysql_fetch_array(mysql_query("SELECT count(id) as smstot from messages "));
+$stock=$stockquery['smstot'];
+return $stock;
+}
+
+function longdate($timestamp)
+{
+return date("l F jS Y", $timestamp);
+}
+
+function customerInfo($var,$id){
+	$info=mysql_fetch_array(mysql_query("SELECT $var from patients where id='$id' "));
+	$var=$info[0];
+	return $var;
+}
+function paidAmount($id)
+{
+	$r=mysql_fetch_array(mysql_query("SELECT sum(amountpaid) as amountpaid from receipts where invoiceid='$id' "));
+	$result=$r['amountpaid'];
+	return $result;
+}
+function unpaidAmount($id)
+{
+	$r=mysql_fetch_array(mysql_query("SELECT amountdue from receipts where invoiceid='$id' order by id desc limit 1 "));
+	$result=$r['amountdue'];
+	return $result;
+}
+function paymentPlan($id){
+		$r=mysql_fetch_array(mysql_query("SELECT planid from invoice_paymentplan where invoiceid='$id' "));
+	$planid=$r['planid'];
+	if ($planid=="0") {
+		$result="CASH";
+	}elseif ($planid=="1") {
+		$result="INSTALLMENT";
+	}
+	return $result;
+}
+?>
